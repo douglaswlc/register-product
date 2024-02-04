696563f711d6fb80462d884ed515eeb59e6169db
Douglas <douglaswlc@gmail.com>
2024-02-04 20:28:16 -0300

    first alter

---
+++ register-product/src/App.js
@@ -0,0 +1,13 @@
+import "./css/index.css";
+import ListProducts from "./js/ListProduct";
+
+function App() {
+  return (
+    <>
+      <h1 className="books">Livros</h1>
+      <ListProducts />
+    </>
+  );
+}
+
+export default App;
